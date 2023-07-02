64a4518d7658b772070500e73033463518b828cd
avik_007 <124508241+aviksain007@users.noreply.github.com>
2023-07-02 20:14:27 +0530

    Create Best_Time_to_Buy_and_Sell_Stock.cpp

---
+++ Array/Easy/Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = 0;
+        int minPrice = prices[0];
+        for(int i=0; i<prices.size(); i++) {
+            if(prices[i] < minPrice) minPrice = prices[i];
+            int profit = prices[i] - minPrice;
+            if(profit > maxProfit) maxProfit = profit;
+        }
+        return maxProfit;
+    }
+};
