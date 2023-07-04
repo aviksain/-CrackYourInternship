7d426ee65c0b1fdff14ccfcaaa753fe5ba20b5db
avik_007 <124508241+aviksain007@users.noreply.github.com>
2023-07-04 23:56:01 +0530

    Create Container_With_Most _Water.cpp

---
+++ Array/Container_With_Most _Water.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int maxArea=0;
+        int left=0, right=n-1;
+        while(left < right) {
+            int area = min(height[left],height[right])*(right-left);
+            maxArea = max(area,maxArea);
+            if(height[left] < height[right]) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+};
