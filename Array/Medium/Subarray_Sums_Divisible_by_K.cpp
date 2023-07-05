31959001cf1e9d84393d936536ebbd07b4bf3af0
avik_007 <124508241+aviksain007@users.noreply.github.com>
2023-07-05 08:35:45 +0530

    Update Subarray_Sums_Divisible_by_K.cpp

--- Array/Medium/Subarray_Sums_Divisible_by_K.cpp
+++ Array/Medium/Subarray_Sums_Divisible_by_K.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int,int> mp;
 
+        mp[0] = 1;
+        int cnt=0;
+        int sum=0;
+
+        for(int i=0; i<n; i++) {
+            sum += nums[i];
+
+            int rem = (sum % k + k) % k;
+
+            if(mp.find(rem) != mp.end()) {
+                cnt += mp[rem];
+            }
+            mp[rem]++;
+        }
+
+        return cnt;
+    }
+};
