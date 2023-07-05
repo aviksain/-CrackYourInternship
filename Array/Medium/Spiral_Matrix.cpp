0d924ba5ffde1e33d2c16e9a6b63bae7145f70a7
avik_007 <124508241+aviksain007@users.noreply.github.com>
2023-07-05 11:58:33 +0530

    Create Spiral_Matrix.cpp

---
+++ Array/Medium/Spiral_Matrix.cpp
@@ -0,0 +1,41 @@
+class Solution {
+private:
+    void func(int n, int m, vector<int>& ans, vector<vector<int>>& matrix) {
+        
+        int l = 0, k = 0;
+
+        while(k<n && l<m) {
+            for(int i=l; i<m; i++) {
+                ans.push_back(matrix[l][i]);
+            }
+            k++;
+            for(int i=k; i<n; i++) {
+                ans.push_back(matrix[i][m-1]);
+            }
+            m--;
+
+            if(k < n) {
+                for(int i=m-1; i>=l; i--) {
+                    ans.push_back(matrix[n-1][i]);
+                }
+                n--;
+            }
+
+            if(l < m) {
+                for(int i=n-1; i>=k; i--) {
+                    ans.push_back(matrix[i][l]);
+                }
+                l++;
+            }
+        }
+    }
+
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int> ans;
+        func(n,m,ans,matrix);
+        return ans;
+    }
+};
