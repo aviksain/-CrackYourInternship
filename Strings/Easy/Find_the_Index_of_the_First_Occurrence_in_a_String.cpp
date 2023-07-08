090af8e04663d8aba27cafc9df8ccb484d214047
avik_007 <124508241+aviksain007@users.noreply.github.com>
2023-07-08 22:57:44 +0530

    Create Find_the_Index_of_the_First_Occurrence_in_a_String.cpp

---
+++ Strings/Easy/Find_the_Index_of_the_First_Occurrence_in_a_String.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.size();
+        int m = needle.size();
+        for(int i=0; i<n; i++) {
+            string str = haystack.substr(i,m);
+            if(str == needle) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
