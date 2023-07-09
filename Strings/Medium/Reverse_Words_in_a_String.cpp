9397d554d47722075bc6887ec8459688b025a41a
avik_007 <124508241+aviksain007@users.noreply.github.com>
2023-07-09 23:44:18 +0530

    Create Reverse_Words_in_a_String.cpp

---
+++ Strings/Medium/Reverse_Words_in_a_String.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string ans;
+        int i = 0;
+        int n = s.length();
+
+        while(i < n){
+            while(i < n && s[i] == ' ') i++;
+            if(i >= n) break;
+            int j = i + 1;
+            while(j < n && s[j] != ' ') j++;
+            string str = s.substr(i, j-i);
+            if(ans.length() == 0) ans = str;
+            else ans = str + " " + ans;
+            i = j+1;
+        }
+        return ans;
+    }
+};
