5e663b0ab3aef501e8cb83ee43477c305c42911d
avik_007 <124508241+aviksain007@users.noreply.github.com>
2023-07-14 20:33:32 +0530

    Update Minimum_Moves_to_Equal_Array_Elements.cpp

--- Mathematical Problems/Easy/Minimum_Moves_to_Equal_Array_Elements.cpp
+++ Mathematical Problems/Easy/Minimum_Moves_to_Equal_Array_Elements.cpp
@@ -1,1 +1,10 @@
-
+class Solution {
+public:
+    int minMoves(vector<int>& nums) {
+        int cnt=0, min = *min_element(nums.begin(),nums.end());
+        for(int i=0; i<nums.size(); i++) {
+            cnt += nums[i]-min;
+        }
+        return cnt;
+    }
+};
