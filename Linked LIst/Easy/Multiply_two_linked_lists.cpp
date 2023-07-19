4ee4ce155c3bafb7f37ac33bd67c12ab7f7df3fb
avik_007 <124508241+aviksain007@users.noreply.github.com>
2023-07-19 13:37:49 +0530

    Create Multiply_two_linked_lists.cpp

---
+++ Linked LIst/Easy/Multiply_two_linked_lists.cpp
@@ -0,0 +1,18 @@
+long long  multiplyTwoLists (Node* l1, Node* l2) {
+    int mod = 1e9+7;
+    
+    long long num1 = 0;
+    long long num2 = 0;
+    
+    while(l1) {
+        num1 = (num1*10)%mod + l1->data;
+        l1 = l1->next;
+    }
+    
+    while(l2) {
+        num2 = (num2*10)%mod + l2->data;
+        l2 = l2->next;
+    }
+    
+    return ((num1%mod)*(num2%mod))%mod;
+}
