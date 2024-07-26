a6a8c38139bcf9a0b4956ebb18c6710b92934f7c
avik <124508241+aviksain@users.noreply.github.com>
2024-07-26 13:00:33 +0530

    Update Subarray_Sums_Divisible_by_K.cpp

--- Array/Medium/Subarray_Sums_Divisible_by_K.cpp
+++ Array/Medium/Subarray_Sums_Divisible_by_K.cpp
@@ -1,24 +1,41 @@
+/*
+    Example: [2,3,-8,-3,11,4,8,6,9,4] k=7
+    prefixSum : [2,5,-3,-6,5,9,17,23,32,36]
+    rem in each step: [2,5,-3,-6,5,2,3,2,4,1]
+        till idx 2 our sum is -3 and from idx 2 to 8 our sum = 32-(-3) = 35 which is divisible by 7 
+        but the rem is not matching -3 != 4 we have to convert this in 4 in order to handel this case
+
+    make the rem positive by rem = rem-k
+                       0 1 2 3 4 5 6 7 8 9
+                      [2,5,4,1,5,2,3,2,4,1]
+    six subarrays are (0,5), (0,7), (1,4), (2,8), (3,9), (5,7) 
+    
+*/
+
+
+
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-        int n = nums.size();
         unordered_map<int,int> mp;
 
         mp[0] = 1;
-        int cnt=0;
-        int sum=0;
+        int ans=0, sum=0;
 
-        for(int i=0; i<n; i++) {
+        for(int i=0; i<nums.size(); i++) {
             sum += nums[i];
+            int rem = sum%k;
 
-            int rem = (sum % k + k) % k;
+            // if our rem is negative the make it positive
+            if(rem < 0) rem += k;
 
             if(mp.find(rem) != mp.end()) {
-                cnt += mp[rem];
+                ans += mp[rem];
             }
+
             mp[rem]++;
         }
 
-        return cnt;
+        return ans;
     }
 };
